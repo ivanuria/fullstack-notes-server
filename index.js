55c41a6a50814bcb4fea4ccc3c3195ce3c3314cc
ivanuria <ivan.uria@gmail.com>
2024-07-24 09:35:20 +0200

    Changed structure

--- index.js
+++ index.js
@@ -1,67 +1,7 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const Note = require("./models/note")
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const PORT = process.env.PORT || 3001
-
-const app = express()
-app.use(cors())
-app.use(express.static("dist"))
-
-
-// CONFIG
-app.use(express.json())
-
-const generateId = () => {
-    return String(Math.max(...notes.map(n => Number(n.id))) + 1)
-}
-
-app.get("/", (request, response) => {
-    response.send("<h1>Hello darkness my old friend</h1>")
-})
-
-app.get("/api/notes", (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
-})
-
-app.post("/api/notes", (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({ error: "Content missing" })
-    }
-    
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
-
-    console.log("note", note)
-    notes = notes.concat(note)
-    response.json(note)
-})
-
-app.get("/api/notes/:id", (request, response) => {
-    const id = request.params.id
-    const note = notes.find(n => String(n.id) === String(id))
-    if (!note) {
-        response.status(404).end()        
-    } 
-    response.json(note)
-})
-
-app.delete("/api/notes/:id", (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(n => String(n.id) !== String(id))
-
-    response.status(204).end()
-})
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,40 +1,40 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log("Give password as argument")
-    process.exit(1)
+  console.log('Give password as argument')
+  process.exit(1)
 }
 
-const username = "ivanuria"
+const username = 'ivanuria'
 const password = process.argv[2]
-const appName = "noteApp"
+const appName = 'noteApp'
 
 const url = `mongodb+srv://${username}:${password}@fullstackopen.9w0q2vh.mongodb.net/${appName}?retryWrites=true&w=majority&appName=FullStackOpen`
 
-mongoose.set("strictQuery", false)
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean
+  content: String,
+  important: Boolean
 })
 
-const Note = mongoose.model("Note", noteSchema)
+const Note = mongoose.model('Note', noteSchema)
 
 /*const note = new Note({
-    content: "MongoDB makes life easier",
-    important: true
+  content: 'MongoDB makes life easier',
+  important: true
 })
 
 note.save().then(result => {
-    console.log("Note saved!")
-    mongoose.connection.close()
+  console.log('Note saved!')
+  mongoose.connection.close()
 })*/
 
-Note.find({_id: "669d6a48ac6cc22ae890f858"}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+Note.find({ _id: '669d6a48ac6cc22ae890f858' }).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
