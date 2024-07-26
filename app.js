215b8d9b0cabd63bab34c7ed6a56a5dffc6cc844
ivanuria <ivan.uria@gmail.com>
2024-07-26 12:53:51 +0200

    added users routers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
+const usersRoute = rquire('./controllers/users')
 const middleware = require('./utils/middleware')
 const { connectMongoDB } = require('./utils/mongodb')
 
@@ -17,6 +18,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRoute)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
